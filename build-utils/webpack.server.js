add48853e827e425d96bbb1f4f8febdc00c6b6cc
trevdevm <trevmunns@gmail.com>
2020-11-29 22:42:12 -0600

    newest prod files

--- build-utils/webpack.server.js
+++ build-utils/webpack.server.js
@@ -3,58 +3,59 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
 const compressWebpack = require("compression-webpack-plugin");
-const LoadablePlugin = require('@loadable/webpack-plugin');
+const LoadablePlugin = require("@loadable/webpack-plugin");
 const nodeExternals = require("webpack-node-externals");
 
 module.exports = () => ({
-    mode: "production",
-    name: "portBack",
-    target: "node",
-    externals: [nodeExternals()],
-    entry: {
-        "improv": "./improv.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: [/(node_modules|bower_components)/, /public/],
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "@babel/preset-react",
-                            "@babel/preset-env"
-                        ]
-                    }
-                }
+  mode: "production",
+  name: "portBack",
+  target: "node",
+  externals: [nodeExternals()],
+  entry: {
+    improv: "./improv.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/(node_modules|bower_components)/, /public/],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "isomorphic-style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
             },
-            {
-                test: /\.css$/,
-                use: ["isomorphic-style-loader",
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true,
-                            importLoaders: 1
-                        }
-                    }]
+          },
+        ],
+      },
+      {
+        test: [/\.(jp?g|png)$/i],
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              publicPath: "/dist/",
+              name: "[name].[ext]",
+              esModule: false,
             },
-            {
-                test: [/\.(jp?g|png)$/i],
-                use: [{
-                    loader: "file-loader",
-                    options: {
-                        publicPath: "/dist/",
-                        name: "[name].[ext]",
-                        esModule: false,
-                    }
-                }]
-            }
-        ]
-    },
-    output: {
-        path: "/home/trevdevm/code/webpack/port_main/server/",
-        filename: "[name].js"
-    }
-})
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: "/home/dynacode/web/port_main/server",
+    filename: "[name].js",
+  },
+});
